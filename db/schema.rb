65770356f9d13681f5507e0876fff984aa236811
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2017-04-03 15:59:50 +0000

    Add generated schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160622154200) do
+ActiveRecord::Schema.define(version: 20170403155048) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "checks", force: :cascade do |t|
+    t.datetime "started_at"
+    t.datetime "ended_at"
+    t.json     "warnings"
+    t.json     "errors"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "jobs", force: :cascade do |t|
+    t.datetime "last_checked_at"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  create_table "links", force: :cascade do |t|
+    t.string   "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -28,5 +49,4 @@ ActiveRecord::Schema.define(version: 20160622154200) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
 end
