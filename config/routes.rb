76d35920b5f18e368f858201eca644c34d323ad6
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-04-04 19:20:32 +0100

    Basic /batch endpoint specs
    An attempt to define the interface and specs for the /batch endpoint.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get "/healthcheck", to: proc { [200, {}, ["OK"]] }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  post "/batch", to: proc { [202, {}, [{}.to_json]] }
+  get "/batch/:id", to: proc { [200, {}, [{}.to_json]] }
 end
