89b4f1f6b8038a3c4e9bf8b43572a54b07a7d7c4
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-04-04 15:14:18 +0100

    Initial /check-link request spec
    This is to try define the HTTP interface somewhat, may change as we
    learn more.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get "/healthcheck", to: proc { [200, {}, ["OK"]] }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get "/check-link", to: proc { [200, {}, [{}.to_json]] }
+
   post "/batch", to: proc { [202, {}, [{}.to_json]] }
   get "/batch/:id", to: proc { [200, {}, [{}.to_json]] }
 end
