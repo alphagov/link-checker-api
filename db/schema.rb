af1f6363297257546df9fac4800b1704ece4fa31
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-04-04 09:21:16 +0100

    Add db schema

---
+++ db/schema.rb
@@ -0,0 +1,32 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20160622154200) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "uid"
+    t.string   "organisation_slug"
+    t.string   "organisation_content_id"
+    t.string   "app_name"
+    t.text     "permissions"
+    t.boolean  "remotely_signed_out",     default: false
+    t.boolean  "disabled",                default: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
