4ccdcfc7d18818ffcce94baf3d7b7f4d1a64db17
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2017-04-06 15:06:21 +0000

    Update environment config property name

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -5,6 +5,6 @@ if ENV["ERRBIT_API_KEY"].present?
     config.project_key = ENV["ERRBIT_API_KEY"]
     config.project_id = 9999 # This is required by airbrake but ignored by errbit.
     config.host = errbit_uri.to_s
-    config.environment_name = ENV["ERRBIT_ENVIRONMENT_NAME"]
+    config.environment = ENV["ERRBIT_ENVIRONMENT_NAME"]
   end
 end
