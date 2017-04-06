aed18f18c38da325ec44db3bef8e596ed9173854
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-04-06 15:30:15 +0100

    Airbrake configuration for 5.8.1
    With the change to 5.8.1 our previous approach of only configuring
    Airbrake if there is a ENV var seems to fail with "undefined method
    `build_notice’ for nil:NilClass" errors - as it expects Airbrake to be
    configured. This adds configuration in for all environments, but the
    environment is ignored if there is not a ERRBIT_API_KEY env var.

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,10 +1,11 @@
-if ENV["ERRBIT_API_KEY"].present?
-  errbit_uri = Plek.find_uri("errbit")
+errbit_uri = Plek.find_uri("errbit")
+environment = ENV.fetch("ERRBIT_ENVIRONMENT_NAME", Rails.env)
 
-  Airbrake.configure do |config|
-    config.project_key = ENV["ERRBIT_API_KEY"]
-    config.project_id = 9999 # This is required by airbrake but ignored by errbit.
-    config.host = errbit_uri.to_s
-    config.environment = ENV["ERRBIT_ENVIRONMENT_NAME"]
-  end
+Airbrake.configure do |config|
+  # we need a key even if errbit isn't used, so fall back to random data
+  config.project_key = ENV.fetch("ERRBIT_API_KEY", SecureRandom.hex(5))
+  config.project_id = 1 # dummy, not used in Errbit
+  config.host = errbit_uri.to_s
+  config.environment = environment
+  config.ignore_environments = ENV["ERRBIT_API_KEY"] ? [environment] : []
 end
