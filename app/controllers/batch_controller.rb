89df02224df58e405cf914ba613376cb505e9603
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-04-07 22:42:51 +0100

    Openstruct validation concept

--- app/controllers/batch_controller.rb
+++ app/controllers/batch_controller.rb
@@ -1,32 +1,12 @@
 class BatchController < ApplicationController
-  class CreateParams
-    include ActiveModel::Validations
-
-    attr_accessor :uris, :checked_within, :webhook_uri
-
-    validates :uris, presence: true, length: { maximum: 5000 }
-    validates :checked_within, numericality: { greater_than: 0 }
-
-    def initialize(params)
-      @params = params
-      @uris = permitted_params[:uris]
-      @checked_within = (permitted_params[:checked_within] || 24.hours).to_i
-      @webhook_uri = permitted_params[:webhook_uri]
-    end
-
-    def permitted_params
-      @permitted_params ||= @params.permit(:checked_within, :webhook_uri, uris: [])
-    end
-  end
+  before_action :validate_create_params, only: [:create]
 
   def create
-    create_params = CreateParams.new(params)
-    create_params.validate!
-
     batch = ActiveRecord::Base.transaction do
-      links = Link.fetch_all(create_params.uris)
-      checks = Check.fetch_all(links, within: create_params.checked_within)
-      Batch.create!(checks: checks, webhook_uri: create_params.webhook_uri)
+      links = Link.fetch_all(create_params[:uris])
+      checked_within = create_params[:checked_within] || 24.hours.to_i
+      checks = Check.fetch_all(links, within: checked_within)
+      Batch.create!(checks: checks, webhook_uri: create_params[:webhook_uri])
     end
 
     if batch.completed?
@@ -50,5 +30,20 @@ private
 
   def batch_report(batch)
     BatchPresenter.new(batch).report
+  end
+
+  def create_params
+    params.permit(:checked_within, :webhook_uri, uris: [])
+  end
+
+  def validate_create_params
+    CreateValidator.new(create_params).validate!
+  end
+
+  class CreateValidator < OpenStruct
+    include ActiveModel::Validations
+
+    validates :uris, presence: true, length: { maximum: 5000 }
+    validates :checked_within, numericality: { greater_than: 0, allow_nil: true }
   end
 end
