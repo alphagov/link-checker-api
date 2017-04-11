7a091b74627f77657088e54595c6ba73cf195784
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2017-04-11 15:35:31 +0100

    Improve error messages
    Thanks to @benjamin-mortimer

--- lib/link_checker/uri_checker/valid_uri.rb
+++ lib/link_checker/uri_checker/valid_uri.rb
@@ -2,6 +2,7 @@ module LinkChecker::UriChecker
   class ValidUri
     HTTP_URI_SCHEMES = %w(http https).freeze
     FILE_URI_SCHEMES = %w(file).freeze
+    OTHER_SCHEMES = %w(mailto tel).freeze
 
     attr_reader :report, :options
 
@@ -14,18 +15,20 @@ module LinkChecker::UriChecker
       parsed_uri = URI.parse(uri)
 
       if parsed_uri.scheme.nil?
-        report.add_error("No scheme", "No scheme given.")
+        report.add_error("Invalid URL", "URLs for external sites must start with 'http://' or 'https://'. If you're linking to a heading on your page, use '#'.")
       elsif HTTP_URI_SCHEMES.include?(parsed_uri.scheme)
         report.merge(HttpChecker.new(parsed_uri, options).call)
       elsif FILE_URI_SCHEMES.include?(parsed_uri.scheme)
         report.merge(FileChecker.new(parsed_uri, options).call)
+      elsif OTHER_SCHEMES.include?(parsed_uri.scheme)
+        report.add_warning("Contact details", "Check these are correct manually.")
       else
-        report.add_warning("Unsupported scheme", "Unsupported scheme.")
+        report.add_warning("Unusual URL", "Check this is meant to be here.")
       end
 
       report
     rescue URI::InvalidURIError
-      report.add_error("URI invalid", "The URI is not valid")
+      report.add_error("Invalid URL", "URL may be missing a forward slash at the beginning or be placeholder text.")
       report
     end
   end
