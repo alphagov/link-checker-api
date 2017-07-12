af87eeaef8e284079fd81fe45ad737b9b6cbbc93
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2017-07-12 14:59:17 +0100

    Add a larger backoff delay on the webhook worker
    This is to avoid DoS-ing client apps.

--- app/workers/webhook_worker.rb
+++ app/workers/webhook_worker.rb
@@ -4,7 +4,13 @@ end
 class WebhookWorker
   include Sidekiq::Worker
 
-  sidekiq_options queue: :webhooks
+  sidekiq_options queue: :webhooks, retry: 5
+
+  sidekiq_retry_in do |count|
+    # retry between 15-20 minutes first, then 30-40 minutes next, then 45-60 minutes next, etc
+    # we have randomness here to make sure that waiting webhooks don't all come in at the same time
+    ((15 + rand(5)) * 60) * (count + 1)
+  end
 
   SIGNATURE_HEADER = "X-LinkCheckerApi-Signature".freeze
 
