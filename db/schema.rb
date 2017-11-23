7ac564d84d770292171fafbf1d883bc2d69fb40a
Tasmin Steer <tasminsteer@hotmail.com>
2017-11-23 12:33:19 +0000

    Remove link errors from monitor links migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171121113257) do
+ActiveRecord::Schema.define(version: 20171121120032) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,7 +62,6 @@ ActiveRecord::Schema.define(version: 20171121113257) do
     t.integer  "link_id"
     t.integer  "resource_monitor_id"
     t.datetime "last_checked_at"
-    t.json     "link_errors"
     t.index ["link_id"], name: "index_monitor_links_on_link_id", using: :btree
     t.index ["resource_monitor_id"], name: "index_monitor_links_on_resource_monitor_id", using: :btree
   end
