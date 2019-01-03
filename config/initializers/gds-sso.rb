1de3335c4aa0dab00d385a7fd3ceede4a7181ad5
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2019-01-03 15:16:30 +0000

    Update signon config for use with bearer tokens
    Pass in a caching adapter cache bearer token requests

--- config/initializers/gds-sso.rb
+++ config/initializers/gds-sso.rb
@@ -3,4 +3,5 @@ GDS::SSO.config do |config|
   config.oauth_id     = ENV["OAUTH_ID"]
   config.oauth_secret = ENV["OAUTH_SECRET"]
   config.oauth_root_url = Plek.find("signon")
+  config.cache = Rails.cache
 end
