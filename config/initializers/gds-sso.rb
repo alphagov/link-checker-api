be35a70ae64d94405ba72ec604065423bf35ba4e
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2019-01-09 15:57:43 +0000

    Use external route for signon

--- config/initializers/gds-sso.rb
+++ config/initializers/gds-sso.rb
@@ -2,6 +2,6 @@ GDS::SSO.config do |config|
   config.user_model   = "User"
   config.oauth_id     = ENV["OAUTH_ID"]
   config.oauth_secret = ENV["OAUTH_SECRET"]
-  config.oauth_root_url = Plek.find("signon")
+  config.oauth_root_url = Plek.new.external_url_for("signon")
   config.cache = Rails.cache
 end
