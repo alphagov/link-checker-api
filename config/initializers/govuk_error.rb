e12e6ac10a49b5cc2f679d4dd8c2ec9241c1be8f
Chris Ashton <ChrisBAshton@users.noreply.github.com>
2021-06-07 12:37:40 +0100

    Revert "Bump govuk app config to version 4.0.0.pre.3"

--- config/initializers/govuk_error.rb
+++ config/initializers/govuk_error.rb
@@ -1,7 +1,6 @@
 GovukError.configure do |config|
-  config.data_sync_excluded_exceptions += %w[
-    Faraday::ServerError
-    Faraday::ConnectionFailed
-    RestartWorkerException
+  config.data_sync_excluded_exceptions += [
+    "Faraday::ServerError",
+    "Faraday::ConnectionFailed",
   ]
 end
