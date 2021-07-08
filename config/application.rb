0f43b37e258567f8ac2aaface493c2dd6d4367c1
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2021-07-08 10:00:31 +0100

    Run `rails app:update` command and Rubocop
    This updates our configuration files to use Rails 6.1 configuration.
    This introduced linting issues, which I fixed by running `rubocop -a`.

--- config/application.rb
+++ config/application.rb
@@ -5,8 +5,11 @@ require "rails"
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
+# require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
 # require "sprockets/railtie"
@@ -21,11 +24,20 @@ module LinkCheckerApi
   self.hosts_with_basic_authorization = {}
 
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-    config.load_defaults = 6.0
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.0
 
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+
+    # Only loads a smaller set of middleware suitable for API only apps.
+    # Middleware like session, flash, cookies can be added back manually.
+    # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
     config.active_job.queue_adapter = :sidekiq
