53a791d5b5406570fc8cf2329dcf3de6d39f3e7c
hannako <jessica.jones@digital.cabinet-office.gov.uk>
2021-07-20 15:59:22 +0100

    Flipping the value of config.load_defaults to be 6.1
    - And removed the temp defaults file as it's no longer needed
    - As per https://edgeguides.rubyonrails.org/upgrading_ruby_on_rails.html#configure-framework-defaults

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module LinkCheckerApi
 
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.0
+    config.load_defaults 6.1
 
     # Configuration for the application, engines, and railties goes here.
     #
