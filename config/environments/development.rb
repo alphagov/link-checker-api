c3f20d17b0af971ff267aad026620a13f4dd6075
Becka Lelew <becka.lelew@digital.cabinet-office.gov.uk>
2022-02-22 10:29:54 +0000

    Bump rails version and run rails app:update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,12 +14,15 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  # Enable server timing
+  config.server_timing = true
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join("tmp/caching-dev.txt").exist?
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      "Cache-Control" => "public, max-age=#{2.days.to_i}",
+      "Cache-Control" => "public, max-age=#{2.days.to_i}"
     }
   else
     config.action_controller.perform_caching = false
