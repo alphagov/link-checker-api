182b8b980ea4db0aac9804b72b404e0a009ceae7
Becka Lelew <becka.lelew@digital.cabinet-office.gov.uk>
2022-02-23 16:50:38 +0000

    Update sidekiq-unique-jobs
    This involves a major version upgrade (from 6.x to 7.x), necessitating a few changes:
    * We now need to explicitly configure the middleware
    * A couple of renamings for the sidekiq option parameters

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,17 @@
+Sidekiq.configure_server do |config|
+  config.client_middleware do |chain|
+    chain.add SidekiqUniqueJobs::Middleware::Client
+  end
+
+  config.server_middleware do |chain|
+    chain.add SidekiqUniqueJobs::Middleware::Server
+  end
+
+  SidekiqUniqueJobs::Server.configure(config)
+end
+
+Sidekiq.configure_client do |config|
+  config.client_middleware do |chain|
+    chain.add SidekiqUniqueJobs::Middleware::Client
+  end
+end
