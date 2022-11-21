30a466b659a12d25d1f4e037192eadf1dd010f38
Kevin Dew <kevindew@me.com>
2022-11-21 16:19:24 +0000

    Resolve lack of Plek#website_uri method
    This method was removed from Plek 5 and follows the update instructions
    from the changelog.

--- config/initializers/hosts_with_basic_authorization.rb
+++ config/initializers/hosts_with_basic_authorization.rb
@@ -1,3 +1,4 @@
 if Rails.application.secrets.govuk_basic_auth_credentials
-  LinkCheckerApi.hosts_with_basic_authorization[Plek.new.website_uri.host.to_s] = Rails.application.secrets.govuk_basic_auth_credentials
+  govuk_website_host = URI(Plek.website_root).host.to_s
+  LinkCheckerApi.hosts_with_basic_authorization[govuk_website_host] = Rails.application.secrets.govuk_basic_auth_credentials
 end
