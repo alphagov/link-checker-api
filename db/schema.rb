6018c722afed473db832a9a0d477a156034f9ef6
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2025-01-23 08:03:10 +0000

    Update database schema for Rails 8

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2017_07_13_101143) do
+ActiveRecord::Schema[8.0].define(version: 2017_07_13_101143) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
 
   create_table "batch_checks", id: :serial, force: :cascade do |t|
     t.integer "batch_id", null: false
