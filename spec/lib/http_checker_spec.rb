e3a1768742dff0de21277e47040858be8ff4781b
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2025-03-18 11:32:21 +0000

    Fetch suspicious domains from DB instead of config
    We're going to add a bunch more suspicious domains to our checks.
    It's not necessarily a good idea to make that list public.
    Therefore we're moving the list of domains into a database (and
    benefiting from increased validation to boot 👢).
    
    Post merge, we'll run this in the Rails console so that we have
    parity with current behaviour:
    
    ```
    SuspiciousDomain.create([
      { domain: "nostringsng.com" },
      { domain: "becauseiamagirl.org" },
      { domain: "bilebrizoua.ci" }
    ])
    ```
    
    Trello: https://trello.com/c/tmnht4P1

--- spec/lib/http_checker_spec.rb
+++ spec/lib/http_checker_spec.rb
@@ -9,9 +9,12 @@ RSpec.describe LinkChecker::UriChecker::HttpChecker do
   end
 
   context "a suspicious domain is passed" do
+    before { SuspiciousDomain.destroy_all }
     let(:uri) { URI("http://malicious.example.com/foo") }
     subject { described_class.new(uri) }
+
     it "should not request the URI if it is a suspicious domain" do
+      SuspiciousDomain.create!(domain: "malicious.example.com")
       expect(subject).not_to receive(:make_request)
       report = subject.call
       expect(report.problems).to match([LinkChecker::UriChecker::SuspiciousDomain])
