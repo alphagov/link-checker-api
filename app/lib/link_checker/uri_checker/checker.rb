e51ead1591bf06f87a15c64084647c9a206d96b6
Bruce Bolt <bruce.bolt@dsit.gov.uk>
2026-02-18 10:44:26 +0000

    Update user agent to a more recent browser and operating system version
    We have identified a number of sites that reject older browsers with a
    403 response code. This results in a false positive link report in
    publishing applications.
    
    Updating the user agent to reduce the likelihood of this.
    
    There is prior art in 7cb47b5ba1fe1d519884e3ffefcd28fd19301589.

--- app/lib/link_checker/uri_checker/checker.rb
+++ app/lib/link_checker/uri_checker/checker.rb
@@ -28,7 +28,7 @@ module LinkChecker::UriChecker
 
     attr_reader :redirect_history
 
-    USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36 (GOV.UK Link Checker API)".freeze
+    USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36 (GOV.UK Link Checker API)".freeze
 
     def client(options = {})
       default_options = { headers: { accept_encoding: "none", user_agent: USER_AGENT } }
